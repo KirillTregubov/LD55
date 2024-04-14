ae66d1e3a71766a1b427f298dd7223cc40b7cc4c
Sean Yang <60932078+s303824@users.noreply.github.com>
2024-04-14 01:50:34 -0400

    added some static art assets

--- Level/court_case.gd
+++ Level/court_case.gd
@@ -1,6 +1,19 @@
 extends Node2D
 
-@export var dialogue: Dialogue
+@export var opening: Dialogue
+@onready var witness_1 = $Subjects/BugWitness
+@onready var circle = $Subjects/SummoningCircle
 
 func _ready():
-	DialogueManager.start_dialogue(dialogue)
+	DialogueManager.scripted_event.connect(handle_scripted_events)
+	DialogueManager.start_dialogue(opening)
+
+
+func handle_scripted_events(event: String):
+	match event:
+		"summon_witness_1":
+			handle_summon1()
+			
+func handle_summon1():
+	circle.start_summoning()
+	witness_1.summon()
